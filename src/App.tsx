97596fd37af5422411774e8ec0ea3789d6e8d6e5
Bozai Cosmin <cosminbozai@outlook.com>
2023-03-08 13:35:24 +0000

    Added Search

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
 import "./App.scss";
+import Search from "./components/Search/Search";
 
 function App() {
   const [theme, setTheme] = useState("light");
   const [font, setFont] = useState("sans-serif");
+  const [definiton, setDefinition] = useState<object | null>({});
   return (
     <div className={`App ${theme} ${font}`}>
       <div className="main-container">
         <Header setTheme={setTheme} setFont={setFont} font={font} />
-        {/* Search  */}
+        <Search setDefinition={setDefinition} />
         {/* Dictionary  */}
       </div>
     </div>
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,51 @@
+import { ReactComponent as SearchIcon } from "../../assets/images/icon-search.svg";
+import axios, { AxiosError } from "axios";
+import { useRef, useState } from "react";
+import "./Search.scss";
+
+type Definiton = {
+  setDefinition: React.Dispatch<React.SetStateAction<object | null>>;
+};
+
+function Search({ setDefinition }: Definiton) {
+  const [empty, setEmpty] = useState(false);
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  const getDefinition = async () => {
+    setEmpty(false);
+
+    const word = inputEl.current?.value;
+    if (word == "") {
+      setEmpty(true);
+      return;
+    }
+    try {
+      const res = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+      );
+      setDefinition(res.data[0]);
+    } catch (err) {
+      const error = err as AxiosError;
+      if (error.code === "404") setDefinition(null);
+    }
+  };
+
+  return (
+    <>
+      <div className="search-wrapper">
+        <input
+          type="search"
+          placeholder="Search for a word"
+          className={`search ${empty ? "error" : ""}`}
+          ref={inputEl}
+        />
+        <SearchIcon className="search-icon" onClick={getDefinition} />
+      </div>
+      <div className="error-msg">
+        {empty ? "Whoops, can't be empty..." : ""}
+      </div>
+    </>
+  );
+}
+
+export default Search;
