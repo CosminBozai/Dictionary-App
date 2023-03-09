66fb36faa56c63616d0db27b46483abf92f63bf3
Bozai Cosmin <cosminbozai@outlook.com>
2023-03-09 18:34:20 +0000

    interfaces for the definitions

---
+++ src/interface/Word.ts
@@ -0,0 +1,23 @@
+interface Definition {
+  antonyms: string[];
+  definition: string;
+  synonyms: string[];
+}
+
+interface Meaning {
+  antonyms: string[];
+  definitions: Definition[];
+  partOfSpeech: "noun";
+  synonyms: string[];
+}
+
+interface Word {
+  license?: object;
+  meanings?: Meaning[];
+  phonetic?: string;
+  phonetics?: object[];
+  sourceUrls?: string[];
+  word: string | null;
+}
+
+export default Word;
